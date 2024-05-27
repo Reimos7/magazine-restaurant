3548f02f7c91561b98304937e4741ff14c8faab5
Reimos7 <tjrvud9@gmail.com>
2024-05-27 17:23:37 +0900

    Initial Commit

---
+++ magazine&restaurant/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  magazine&restaurant
+//
+//  Created by 홍석평 on 5/27/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
