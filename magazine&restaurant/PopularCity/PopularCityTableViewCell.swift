c520ee29abdb0500b0985497f7e66144d72ebb53
Reimos7 <tjrvud9@gmail.com>
2024-06-04 00:39:07 +0900

    여행미니 프로젝트(인기도시)  SegmentedControl 기능 및 디자인 구현 완료

--- magazine&restaurant/PopularCity/PopularCityTableViewCell.swift
+++ magazine&restaurant/PopularCity/PopularCityTableViewCell.swift
@@ -19,11 +19,28 @@ class PopularCityTableViewCell: UITableViewCell {
     override func awakeFromNib() {
         super.awakeFromNib()
         
+        // UILabel의 왼쪽 상단과 오른쪽 하단 모서리를 둥글게 만들고, 반지름이 20인 둥근 모서리를 설정하며, 경계를 벗어나는 부분을 잘라줌
+        popularCityImage.layer.maskedCorners = [.layerMinXMinYCorner, .layerMaxXMaxYCorner]
+        popularCityImage.layer.cornerRadius = 20
+        popularCityImage.clipsToBounds = true
+        popularCityImage.contentMode = .scaleAspectFill
+        
+        popularCityTitleLabel.textAlignment = .right
+        popularCityTitleLabel.textColor = .white
+        popularCityTitleLabel.font = .boldSystemFont(ofSize: 17)
+        
+        popularCityListLabel.textColor = .white
+        popularCityListLabel.backgroundColor = UIColor.black.withAlphaComponent(0.4)
+        
     }
 
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
+    func configureCell(data: City){
+        let url = URL(string: data.city_image)
+        popularCityImage.kf.setImage(with: url)
 
+        popularCityTitleLabel.text = data.titleNames
+        
+        popularCityListLabel.text = data.city_explain
         
     }
     
--- magazine&restaurant/PopularCity/PopularCityViewController.swift
+++ magazine&restaurant/PopularCity/PopularCityViewController.swift
@@ -8,31 +8,95 @@
 import UIKit
 
 class PopularCityViewController: UIViewController {
-
+    
     
     @IBOutlet var tableView: UITableView!
     
+    // 도시 정보
+    var list = CityInfo.city
+    
+    // 도시 빈배열
+    var filterList: [City] = []
+    
+    // 한국 도시 정보 빈배열
+    var filterKoreaList: [City] = []
+    
+    // 해외 도시 정보 빈배열
+    var filterForeignList: [City] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        self.title = "인기 도시"
         tableView.dataSource = self
         tableView.delegate = self
+        tableView.rowHeight = 120
+        
+        // xib 사용
+        let xib = UINib(nibName: "PopularCityTableViewCell", bundle: nil)
+        tableView.register(xib, forCellReuseIdentifier: "PopularCityTableViewCell")
         
     }
     
-
-   
+    
+    // 세그먼트 컨트롤
+    @IBAction func segmentedControlClicked(_ sender: UISegmentedControl) {
+        
+        // 모두를 클릭했을때
+        if sender.selectedSegmentIndex == 0 {
+            
+            // 빈배열에 모든 정보 저장
+            filterList = list
+            tableView.reloadData()
+            
+        }else if sender.selectedSegmentIndex == 1 {
+         
+            for item in list {
+                
+                if item.city_name == "제주" || item.city_name == "부산" || item.city_name == "전주" || item.city_name == "서울" || item.city_name == "대전" {
+                    
+                    filterKoreaList.append(item)
+                }
+            }
+            filterList = filterKoreaList
+            tableView.reloadData()
+            
+            
+        } else {
+      
+            for item in list {
+                
+                let cityName = item.city_name
+                
+                if cityName == "방콕" || cityName == "오사카" || cityName == "다낭" || cityName == "파리" || cityName == "시드니" || cityName == "밀라노" || cityName == "리스본" || cityName == "두바이" || cityName == "홍콩" {
+                    
+                    filterForeignList.append(item)
+                }
+            }
+            filterList = filterForeignList
+            tableView.reloadData()
+        }
+        
+    }
+    
 }
 extension PopularCityViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return filterList.count
+        
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PopularCityTableViewCell", for: indexPath) as! PopularCityTableViewCell
+        
+        let data = filterList[indexPath.row]
+        
+        cell.configureCell(data: data)
+        
+        return cell
     }
-    
     
 }
 
